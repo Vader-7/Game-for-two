6a7301e40278e0669db66c500cd84a93b6829227
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-08-29 16:23:37 -0400

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'hayashi.labs',
   appName: 'App',
   webDir: 'www',
   bundledWebRuntime: false
